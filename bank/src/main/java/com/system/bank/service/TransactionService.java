12557c40544a671899051c3a23054d4f494f77fe
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2023-12-11 13:49:03 -0400

    Aplicação Web de conta bancaria
    Aplicação Web de banco digital construido com Java, Rest, banco de dados MySQL, SpringBoot e Postman para testes

--- bank/src/main/java/com/system/bank/service/TransactionService.java
+++ bank/src/main/java/com/system/bank/service/TransactionService.java
@@ -51,8 +51,8 @@ public class TransactionService {
         this.notificationService.sendNotification(sender, "Transação realizada com sucesso");
         this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
 
-
         return newtransacao;
+
     }
     public boolean authorizeTransaction(User sender, BigDecimal valor){
       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc",Map.class);
